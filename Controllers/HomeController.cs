74d8f53d1f58b152f92d56b0370afbdae4145ac6
LuyoloLuja <luyolo.mntuyedwa97@gmail.com>
2024-08-16 20:35:02 +0200

    inital commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ContactForm.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ContactForm.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new DetailsViewModel());
+    }
+
+    [HttpPost]
+    public IActionResult Index(DetailsViewModel model) 
+    {
+        if(!ModelState.IsValid && model.IsChecked == false) {
+            return View(model);
+        }
+        // return BadRequest(ModelState);
+        return PartialView("_SuccessModal", model);
+    }
+}
---
+++ Models/DetailsViewModel.cs
@@ -0,0 +1,45 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactForm.Models;
+
+public class DetailsViewModel
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "This field is required")]
+    [Display(Name = "First Name *")]
+    [StringLength(50)]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "This field is required")]
+    [Display(Name = "Last Name *")]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "This field is required")]
+    [Display(Name = "Email Address *")]
+    [StringLength(50)]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "To submit this form, please consent to being contacted")]
+    [Display(Name = "I consent to being contacted by the team")]
+    public bool IsChecked { get; set; }
+
+    [Required(ErrorMessage = "This field required")]
+    [Display(Name = "Message *")]
+    public string Message { get; set; }
+
+    [Required(ErrorMessage = "Please select a query type")]
+    [Display(Name = "Query Type *")]
+    public QueryType SelectQueryType { get; set; }
+}
+
+public enum QueryType
+{
+    [Display(Name = "General Enquiery")]
+    GeneralEnquiery,
+
+    [Display(Name = "Support Request")]
+    SupportRequest
+}
