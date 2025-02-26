4bc820d29409c6fddece99dce984f7cb54d9d751
LuyoloLuja <luyolo.mntuyedwa97@gmail.com>
2025-02-26 06:24:44 +0200

    running migrations automatically on program.cs

--- Program.cs
+++ Program.cs
@@ -20,6 +20,13 @@ builder.Services.AddDbContext<ContactFormDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ContactFormDbContext>();
+    dbContext.Database.Migrate();
+}
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
